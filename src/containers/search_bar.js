9049358da187a4f2aefb129604b3a1624122e50a
Kurt Schneider <k.w.schneid@gmail.com>
2017-12-11 09:52:27 -0500

    Adding state for cities

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { term: '' };
 
 		this.onInputChange = this.onInputChange.bind(this)
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 
 	onInputChange(e) {
@@ -14,6 +18,9 @@ export default class SearchBar extends Component {
 
 	onFormSubmit(e) {
 		e.preventDefault();
+
+		this.props.fetchWeather(this.state.term);
+		this.setState({term: ''});
 	}
 
 	render() {
@@ -32,3 +39,9 @@ export default class SearchBar extends Component {
 		)
 	}
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({fetchWeather},dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
