f8fb117d225de188e5caa405fe6dd1d989d268c2
Kurt Schneider <k.w.schneid@gmail.com>
2017-12-13 10:00:17 -0500

    Iunno doin stufF

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,10 @@
-export default function(state = null, action) {
-	console.log('Action Received',action);
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action) {
+	switch (action.type) {
+	case FETCH_WEATHER:
+		// create new state to return instead of manipulate old state, same as you should never set state = something, should always set state with setState
+		return [ action.payload.data, ...state ]; // [city, city, city]
+	}
 	return state;
 }
